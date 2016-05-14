cbd61b2589d0443a067bbb38ae8fd89772fd5aab
StasBel <stasbelyaev96@gmail.com>
2016-05-14 16:39:17 +0300

    add distinguished class for word analizator

--- src/main/pymorph_nf.py
+++ src/main/pymorph_nf.py
@@ -1,35 +1,33 @@
 from pymorphy2 import MorphAnalyzer
 
-morph = MorphAnalyzer()
 
+class Parser:
+    def __init__(self, min_accept_score=0.2):
+        self.morph = MorphAnalyzer()
+        self.min_accept_score = min_accept_score
 
-class NormalFormReturnValue(object):
-    def __init__(self, candidates, is_in_dict):
-        self.candidates = candidates
-        self.is_in_dict = is_in_dict
+    def is_in_ruscorpra(self, word):
+        return self.morph.word_is_known(word)
 
+    class NormalFormReturnValue(object):
+        def __init__(self, candidates):
+            self.candidates = candidates
 
-def is_in_ruscorpra(word):
-    return morph.word_is_known(word)
-
-
-MIN_ACCEPT_SCORE = 0.20
-
-
-def normal_form(word):
-    parse = morph.parse(word.lower())
-    result = []
-    result_set = set()
-    for var in parse:
-        if (not result) or (var.score > MIN_ACCEPT_SCORE and var.normal_form not in result_set):
-            result_set.add(var.normal_form)
-            result.append(var.normal_form)
-    return NormalFormReturnValue(result, is_in_ruscorpra(result[0]))
+    def normal_form(self, word):
+        parse = self.morph.parse(word.lower())
+        result = []
+        result_set = set()
+        for var in parse:
+            if (not result) or (var.score > self.min_accept_score and var.normal_form not in result_set):
+                result_set.add(var.normal_form)
+                result.append(var.normal_form)
+        return self.NormalFormReturnValue(result)
 
 
 if __name__ == '__main__':
+    parser = Parser()
     name_word = 'вэжэвания'
-    print(normal_form(name_word).candidates)
-    print(is_in_ruscorpra(name_word))
-    for parse_var in morph.parse(name_word):
+    print(parser.normal_form(name_word).candidates)
+    print(parser.is_in_ruscorpra(name_word))
+    for parse_var in parser.morph.parse(name_word):
         print(parse_var)
